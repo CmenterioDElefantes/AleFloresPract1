0f216cd4146b7fac1975ee62b34af6a60f2ae03a
CmenterioDElefantes <sis421ale@gmail.com>
2021-04-15 19:23:22 -0400

    preg4

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,9 @@
-let array:number[]=[1,0,1,0,1000]; ////[-1,-2]  ///[3,6,-2,-5,7,3];
+let pieck:number[]=[0,3] ///[5,4,6]        ///[6,2,3,8];
+pieck.sort();
 let n:number;
 let aux:number=0;
-for(n=0;n<array.length;n++){
- if (array[n]*array[n+1]>aux)
-        aux=array[n]*array[n+1];
+for(n=0;n<pieck.length;n++){
+ if (pieck[n+1]-pieck[n]>1)
+        aux=aux+pieck[n+1]-pieck[n]-1;
 }
 console.log(aux);
