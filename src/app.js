2f87cd833af23adae261dbdd181f118da7025f82
CmenterioDElefantes <sis421ale@gmail.com>
2021-04-15 18:34:28 -0400

    preg2

--- src/app.js
+++ src/app.js
@@ -1,11 +1,2 @@
-function esPrimo(numero) {
-    if (numero == 0 || numero == 1 || numero == 4)
-        return false;
-    for (var x = 2; x < numero / 2; x++) {
-        if (numero % x == 0)
-            return false;
-    }
-    return true;
-}
-var numero = 7;
-console.log(esPrimo(numero));
+var cadena = "bang";
+console.log(cadena.split('').reverse().join(''));
