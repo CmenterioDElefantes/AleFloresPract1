a99460688c01629cc68ccfe89868d4c5daa06e4e
CmenterioDElefantes <sis421ale@gmail.com>
2021-04-15 19:17:19 -0400

    preg4

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
-var cadena = "bangnab";
-if (cadena == (cadena.split('').reverse().join('')))
-    console.log(true);
-else
-    console.log(false);
+var array = [1, 0, 1, 0, 1000]; ////[-1,-2]  ///[3,6,-2,-5,7,3];
+var n;
+var aux = 0;
+for (n = 0; n < array.length; n++) {
+    if (array[n] * array[n + 1] > aux)
+        aux = array[n] * array[n + 1];
+}
+console.log(aux);
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
-let cadena="bangnab";
-if(cadena==(cadena.split('').reverse().join(''))
-console.log(true);
-else console.log(false);
+let array:number[]=[1,0,1,0,1000]; ////[-1,-2]  ///[3,6,-2,-5,7,3];
+let n:number;
+let aux:number=0;
+for(n=0;n<array.length;n++){
+ if (array[n]*array[n+1]>aux)
+        aux=array[n]*array[n+1];
+}
+console.log(aux);
